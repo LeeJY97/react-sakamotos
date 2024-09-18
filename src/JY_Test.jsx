286ccefedc245c9ab09d1979249a94c72b1fbe53
LeeJY97 <ijoonyeol97@gmail.com>
2024-09-19 02:24:21 +0900

    feat: 커스텀css

--- src/JY_Test.jsx
+++ src/JY_Test.jsx
@@ -12,12 +12,30 @@ const JY_Test = () => {
           showToast({
             message: "top-right",
             position: "top-right",
-            time: 50000,
+            time: 5000,
             showProgress: true,
             bg: true,
             theme: "warning",
             confirm: (confirm) => {
               // console.log("confirm", confirm);
+            },
+            custom: {
+              box: {
+                background: "#333",
+                // width: "100px",
+                // height: "100px",
+              },
+              contents: {
+                color: "red",
+                fontSize: "40px",
+                imgUrl:
+                  "https://item.kakaocdn.net/do/493188dee481260d5c89790036be0e664022de826f725e10df604bf1b9725cfd",
+              },
+              progress: {
+                background: "#b35fc4",
+                height: "10px",
+                width: "10%",
+              },
             },
           })
         }
