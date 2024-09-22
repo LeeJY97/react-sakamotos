9a43f37ab02ffc635cf5333c54962dc9c98ac9a4
LeeJY97 <ijoonyeol97@gmail.com>
2024-09-22 18:37:02 +0900

    update: 파일명 전부 수정, main.jsx, index.html 수정

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import ToastPortal from "./toast/Toast.jsx";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <>
       <ToastPortal />
